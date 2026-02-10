cc94f2f2e00bb9f1f3885b9a20d23dd187fe123f
lztyblog <z1803315755@gmail.com>
2026-02-10 10:12:55 +0000

    fix fetch for vercel

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/hello", {
+  const res = await fetch("api/hello", {
     cache: "no-store",
   });
   const data = await res.json();
